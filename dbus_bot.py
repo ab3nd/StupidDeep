a5ce9768eeb3b82fe0dadf84a421d51816e2de0f
Abe Shultz <orphrey@gmail.com>
2016-04-18 11:01:43 -0400

    Well, it works. By which I mean "It yells 'Insect! I cannot bear your words! They are TOO TINY!" at my friends".

--- dbus_bot.py
+++ dbus_bot.py
@@ -1,52 +1,37 @@
+#!/usr/bin/python
 '''
 Created on Nov 18, 2011
 Watches pidgin over Dbus and responds to incoming messages. 
 '''
 
 import dbus 
-import util
 from dbus.mainloop.glib import DBusGMainLoop
 import gobject
 from pickle import Unpickler
 import random
 import re
 
+aliases = ["Me", "orphrey@gmail.com/[A-F0-9]{8}", "ab3nd", "honest.abe.robot@gmail.com/[A-F0-9]{8}"]
+
+def isMe(name):
+    for alias in aliases:
+        if re.match(alias, name):
+            return True
+    return False
+
+
 class StupidResponder():
     def __init__(self):
-        #Load the file of all conversations
-        inFile = open("./all_convos.pickle")
-        all_convos = Unpickler(inFile).load()
-        self.callResp = {}
-        
-        #Build a dictionary of what they said and my response
-        for convo in all_convos:
-            for ii in range(0, len(convo)-2):
-                if not util.isMe(convo[ii][0]) and util.isMe(convo[ii+1][0]):
-                    #Found a pair of lines with their line and my response
-                    call = convo[ii][1].lower()
-                    call = util.cleanRefs(call)
-                    response = convo[ii+1][1]
-                    if(len(call.split()) < 5):
-                        if not call in self.callResp.keys():
-                            self.callResp[call] = [util.cleanRefs(response)]
-                        else:
-                            self.callResp[call].append(util.cleanRefs(response))
+        #This is where you'd load the model and whatnot. 
+        pass
 
     def getResponse(self, message):
-        #Clean up to match keys
-        message = message.lower()
-        message = re.sub("<[^>]*>", "", message)
-        message = util.cleanRefs(message)
-        #Return random message
-        if message in self.callResp.keys():
-            return random.choice(self.callResp[message])
         #No idea what to say
-        #return "Insect! I cannot bear your words! They are TOO TINY!"
-        return None
-        
+        return "Insect! I cannot bear your words! They are TOO TINY!"
     
     
 def got_msg_cb(account, sender, message, conversation, flags):
+    print sender
     reply = responder.getResponse(message)
     if reply != None:
         purple.PurpleConvImSend(purple.PurpleConvIm(conversation), reply)
